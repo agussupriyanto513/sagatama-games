5dd9ca0c9c2138df6d053099596b4130b5fb31f4
Agus Supriyanto <satulimatiga79@gmail.com>
2026-07-13 07:21:40 +0700

    Add files via upload

--- api/payments/complete.js
+++ api/payments/complete.js
@@ -1,16 +1,7 @@
 // /api/payments/complete.js
 // Complete Pi payment + tambah SGT ke saldo player di Firestore
-import admin from "firebase-admin";
-
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId:   process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey:  process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n')
-    })
-  });
-}
+import { admin, getFirebaseApp } from "../../firebase-init.js";
+getFirebaseApp();
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
